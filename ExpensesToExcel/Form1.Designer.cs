1173d7c7a5a5a82b890f87684dbc4a099f187f89
Paul Ikeda <paul.ikeda@sigmanest.com>
2020-04-17 21:37:44 -0700

    Initial commit

---
+++ ExpensesToExcel/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ExpensesToExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(14, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(469, 91);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Drop file here";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.DragDrop += new System.Windows.Forms.DragEventHandler(this.label1_DragDrop);
+            this.label1.DragEnter += new System.Windows.Forms.DragEventHandler(this.label1_DragEnter);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Personal Capital to Excel Spending Per Day of the Month"});
+            this.comboBox1.Location = new System.Drawing.Point(14, 108);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(374, 23);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(14, 140);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(468, 367);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(395, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 27);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 519);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Expenses To Excel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ExpensesToExcel/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ExpensesToExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        private void label1_DragDrop(object sender, DragEventArgs e)
+        {
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files != null && files.Length != 0)
+            {
+                label1.Text = files[0];
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(label1.Text))
+                return;
+
+            var listOfTransactions = new List<Transaction>();
+
+
+            var lines = File.ReadAllLines(label1.Text);
+
+            for (var i = 1/*skip header*/; i < lines.Length; i++)
+            {
+                var lineArray = lines[i].Split(',');
+
+                var transaction = new Transaction();
+
+                DateTime.TryParse(lineArray[0], out var date);
+                transaction.Date = date;
+
+                decimal.TryParse(lineArray[4], NumberStyles.Any,
+                    new CultureInfo("en-US"), out var number);
+
+                transaction.Amount = number * -1; //make spending positive
+                if (transaction.Amount <= 0)
+                    continue;
+
+                listOfTransactions.Add(transaction);
+            }
+
+            var dayOfTheMonthSumList = listOfTransactions.GroupBy(x => new { x.Date })
+                .Select(a => new { Sum = a.Sum(b => b.Amount), DayOfTheMonth = a.Key })
+                .ToList();
+            dayOfTheMonthSumList.Reverse();
+
+            var outputLine = new List<string>();
+
+            var previousMonth = 0;
+            decimal runningMonthSum = 0;
+            foreach (var dayOfTheMonth in dayOfTheMonthSumList)
+            {
+
+                if (dayOfTheMonth.DayOfTheMonth.Date.Month != previousMonth)
+                {
+                    previousMonth = dayOfTheMonth.DayOfTheMonth.Date.Month;
+                    runningMonthSum = 0;
+                    outputLine.Add($"{CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(dayOfTheMonth.DayOfTheMonth.Date.Month)},Day of the Month,Running Total,Daily Sum");
+                }
+
+                runningMonthSum += dayOfTheMonth.Sum;
+                outputLine.Add($"{dayOfTheMonth.DayOfTheMonth.Date.ToShortDateString()},{dayOfTheMonth.DayOfTheMonth.Date.Day},{runningMonthSum},{dayOfTheMonth.Sum}");
+
+            }
+
+            textBox1.Lines = outputLine.ToArray();
+        }
+    }
+
+    internal class Transaction
+    {
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
